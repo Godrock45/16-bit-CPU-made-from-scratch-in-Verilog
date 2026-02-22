1d3005bdc4a9f67915e5717e235db164d75c4ba2
Akshat Singh <akshat2045@gmail.com>
2026-02-22 13:24:18 -0500

    Refactor instr_rom256 and add data_mem256x16 module

--- instr_rom.v
+++ instr_rom.v
@@ -15,3 +15,21 @@ module instr_rom256(
     end
 
 endmodule
+
+module data_mem256x16 (
+    input logic clk,
+    input logic writeEna,
+    input logic [15:0] address,
+    input logic [15:0] writeData
+    output logic [15:0] readData);
+
+    logic [15:0] mem [0:255];
+
+    always_ff @(posedge clk) begin
+        readData<=mem[address[7:0]];
+        if(writeEna)begin
+            mem[address[7:0]]<=writeData;
+        end
+    end
+endmodule
+    
