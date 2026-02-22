265761bf412004aad28d23f574870f4584d55bd7
Akshat Singh <akshat2045@gmail.com>
2026-02-22 13:30:31 -0500

    Refactor Instruction_register.v and add signnext_inm6to16
    Added signnext_inm6to16 module to extend 6-bit immediate to 16-bit.

--- Instruction_register.v
+++ Instruction_register.v
@@ -23,4 +23,13 @@ assign rs1=destructo[11:9];
 assign rs2=destructo[8:6];
 assign rd=destructo[5:3];
 assign imm= destructo[5:0];
+
 endmodule
+
+module signnext_inm6to16(
+    input logic[5:0] imm6,
+    output logic [15:0] imm16
+);
+    assign imm16= {{10{imm6[5]}},imm6);
+endmodule
+                   
