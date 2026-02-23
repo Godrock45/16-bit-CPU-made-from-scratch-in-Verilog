30b6ee23556ffa2bc803df5fd5924651e2a75530
Akshat Singh <akshat2045@gmail.com>
2026-02-23 04:03:52 -0500

    Update alu.v

--- alu.v
+++ alu.v
@@ -9,24 +9,27 @@ module ALU_Project(
     output reg [15:0] res
     //final result
 );
-wire signed [15:0] lhs_sign=LHS;
-wire signed [15:0] rhs_sign=RHS;
+wire signed [15:0] lhs_sign,rhs_sign;
+
 
 //function calls for respective methods and operations
-always @(*) begin
+always_comb begin
+    lhs_sign = LHS;
+    rhs_sign = RHS;
      case(opp)
      3'b000 : res= LHS&RHS;   // AND
      3'b001 : res=LHS|RHS;   // OR
-     3'b010 : res= sum;     //ADD
+     3'b010 : res= LHS + RHS     //ADD
      3'b011 : res=~LHS;      //NOT
      3'b100 : res=LHS-RHS;  //SUB using 2's complement
      3'b101 : res= LHS^RHS;       //XOR
-     3'b110 : res=LHS<RHS;       // set less than
+     3'b110 : res=lhs_sign<rhs_sign?16'd1:16'd0;       // set less than
      3'b111 : res= ~(LHS&RHS); // NAND
      default : res= 16'hDEAD; //Error 
      endcase
 end
 endmodule
+
 
 
 
