3f9d84049a26068e3311f9b71f37195a98631042
Akshat Singh <akshat2045@gmail.com>
2026-02-23 13:26:28 -0500

    Refactor cpu_16multcycle_readable module

--- control.v
+++ control.v
@@ -10,19 +10,19 @@ logic [15:0] regB;
 logic [15:0] aluOut;
 	logic [15:0] DataMemRead;
 logic [15:0] fetch_inst;
-instr_rom256 u_rom(.pc(pc), .instruction(fetchedInstruction));
+	instr_rom256 u_rom(.pc(pc), .instruction(fetch_inst));
 logic [3:0] opcode;
 logic [2:0] rs1, rs2, rd;
 logic [5:0] imm6;
-decode u_dec(.instruction(ir),.opcde(opcode),.rs1(rs1),.rd(rd),.imm6(imm6));
-logic [15:0] imm6;
-signext_imm6_to15 )inm(.imm6(imm6),.imm16(imm16));
+decode u_dec(.instruction(ir),.opcode(opcode), .rs1(rs1), .rs2(rs2) .rd(rd), .imm6(imm6));
+logic [15:0] imm16;
+	signext_imm6_to15 u_imm(.imm6(imm6),.imm16(imm16));
 logic [15:0] rfRead1, rfRead2;
 logic rfWriteEnable;
 logic [2:0] rfWriteAddr;
 logic [15:0] rfWriteData;
 
-regfile8x16 u_rf(.clk(clk), .rst(rst), .readAddr1(rs1), .readData2(rfRead2), .writeEnable(rfWriteEnable), .writeAddr(rfWriteAddr), .writeData(rfWriteData));
+	regfile8x16 u_rf(.clk(clk), .rst(rst), .readAddr1(rs1), .readAddr2(rs2), .readData1(rfRead1), .readData2(rfRead2), .writeEnable(rfWriteEnable), .writeAddr(rfWriteAddr), .writeData(rfWriteData));
 logic [15:0] aluInA, aluInB, aluResult;
 logic aluZero;
 logic [2:0] aluCtrl;
@@ -84,7 +84,7 @@ always_comb begin
 	aluCtrl= opcode[2:0];
 	if(state==MEM && isStore)
 		dataMemWriteEnable=1'b1;
-	if(state==WRITEBACK && (isRtype ||isLoad))
+	if(state==WRITEBACK && (isRtype||isLoad))
 		rfWriteEnable =1'b1;
 		rfWriteAddr=rd;
 		rfWriteData= isRtype?aluOut:DataMemRead;
@@ -97,7 +97,7 @@ always_ff @(posedge clk) begin
 		regA<=16'h000;
 		regB<=16'h0000;
 		aluOut <=16'h0000;
-		memDataReg<=16'h0000;
+		DataMemReg<=16'h0000;
 	end
 	else begin
 		case(state)
@@ -131,7 +131,7 @@ always_ff @(posedge clk) begin
 			end
 			WRITEBACK: begin
 			end
-			default;
+			default: ;
 		endcase
 	end
 end
@@ -145,6 +145,7 @@ endmodule
             
             
             
+
 
 
 
