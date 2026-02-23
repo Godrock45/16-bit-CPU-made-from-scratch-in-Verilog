e5aadd768e3c0b5254f5aa903a0539b41290b1fb
Akshat Singh <akshat2045@gmail.com>
2026-02-23 03:48:30 -0500

    Refactor cpu_16multcycle_readable module

--- control.v
+++ control.v
@@ -26,12 +26,123 @@ regfil8x16 u_rf(.clk(clk), .rst(rst), .readAddr1(rs1), .readData2(rfRead2), .wri
 logic [15:0] aluInA, aluInB, aluResult;
 logic aluZero;
 logic [2:0] aluCtrl;
-alu16 u_alu(.opA(aluInA), .opB(aluInB), .(aluCtrl(aluCtrl), .aluResult(aluResult), .isZero(aluZero));
+alu16 u_alu(.opA(aluInA), .opB(aluInB), .aluCtrl(aluCtrl), .aluResult(aluResult), .isZero(aluZero));
 logic dataMemWriteEnable;
 logic [15:0] dataMemReadData;
+data_mem256x16 u_dmem(.clk(clk), .writeEnable(dataMemWriteEnable), .address(aluOut), .writeData(regB), .readData(dataMemReadData));
+
+parameter FETCH =0, DECODE= 1, EXECUTE=2, MEM=3, WRITEBACK=4;
+reg [2:0] state, nextState;
+logic isRtpe, isLoad, isStore, isBEQ, isBNE;
+always_comb begin
+	isRtype = (opcode<=4'h7);
+	isLoad = (opcode == 4'h8);
+	isStore = (opcode == 4'h9);
+	isBeQ = (opcode ==4'hA);
+	isBNE = (opcode==4'hB);
+end
+
+always_comb begin
+	nextState=state;
+	case (state)
+		FETCH: nextState = DECODE;
+		DECODE: nextState = EXECUTE;
+		EXECUTE:begin
+			if(isLoad||isStore)
+				nextState=MEM;
+			else if(isRtype)
+				nextState=WRITEBACK;
+			else if(isBEQ || isBNE)
+				nextState= FETCH;
+			else
+				nextState= FETCH;
+		end
+		MEM: begin
+			if(isLoad)
+				nextState= WRITEBACK;
+			else 
+				nextState= FETCH;
+		end
+		WRITEBACK: nextState=FETCH;
+		default: nextState=FETCH;
+	endcase
+end
+always_ff@(posedge clk)begin
+	if(rst)
+		state<=FETCH;
+	else
+		state<=nextState;
+end
+
+always_comb begin
+	rfWriteEnable =1'b0;
+	rfWriteAddr	= rd;
+	rfWriteData = 16'h0000;
+	dataMemWriteEnable = 1'b0;
+	aluInA= regA;
+	aluInB= regB;
+	aluCtrl= opcode[2:0];
+	if(state==MEM && isStore)
+		dataMemWrite=1'b1;
+	if(state==WRITEBACK && (isRtype ||isLoad))
+		rfWriteEnable =1'b1;
+		rfWriteAddr=rd;
+		rfWriteData= isRtype?alOut:memDataReg;
+end
+
+always_ff @(posedge clk) begin
+	if(rst)begin
+		pc<=16'h0000;
+		ir<=16'h0000;
+		regA<=16'h000;
+		regB<=16'h0000;
+		aluOut <=16'h0000;
+		memDataReg<=16'h0000;
+	end
+	else begin
+		case(state)
+			FETCH: begin
+				ir<= fetch_inst;
+				pc<=pc+16'd1;
+			end
+			DECODE:begin
+				regA<=rfRead1;
+				regB<=rfRead2;
+			end
+			EXECUTE:begin
+				if(isRtype)begin
+					aluOut<=aluResult;
+				end
+				else if(isLoad||isStore) begin
+					aluOut<= regA +imm16;
+				end
+				else if(isBEQ || isBNE)begin
+					logic equal;
+					logic takeBranch;
+					equal=(regA==regB);
+					takeBranch=isBEQ?equal:~equal;
+					if(takeBranch)
+						pc<=pc+imm16;
+				end
+			end
+			MEM:begin
+				if(isLoad)
+					memDataRegM<=dataMemReadData;
+			end
+			WRITEBACK: begin
+			end
+			default;
+		endcase
+	end
+end
+endmodule
+
+	   
+			
 
 					
             
             
             
             
+
