6584b87eca1ab8f7ec93b8feebff78904d1ea373
Akshat Singh <akshat2045@gmail.com>
2026-02-25 01:28:31 -0500

    Refactor cpu_16multcycle_readable module

--- control.sv
+++ control.sv
@@ -8,21 +8,21 @@ logic [15:0] ir;
 logic [15:0] regA;
 logic [15:0] regB;
 logic [15:0] aluOut;
-	logic [15:0] DataMemRead;
+logic [15:0] DataMemRead;
 logic [15:0] fetch_inst;
-	instr_rom256 u_rom(.pc(pc), .instruction(fetch_inst));
+instr_rom256 u_rom(.pc(pc), .instruction(fetch_inst));
 logic [3:0] opcode;
 logic [2:0] rs1, rs2, rd;
 logic [5:0] imm6;
-	decode u_dec(.instruction(ir),.opcode(opcode), .rs1(rs1), .rs2(rs2), .rd(rd), .imm6(imm6));
+decode u_dec(.instruction(ir),.opcode(opcode), .rs1(rs1), .rs2(rs2), .rd(rd), .imm6(imm6));
 logic [15:0] imm16;
-	signext_imm6_to16 u_imm(.imm6(imm6),.imm16(imm16));
+signext_imm6_to16 u_imm(.imm6(imm6),.imm16(imm16));
 logic [15:0] rfRead1, rfRead2;
 logic rfWriteEnable;
 logic [2:0] rfWriteAddr;
 logic [15:0] rfWriteData;
 
-	regfile8x16 u_rf(.clk(clk), .rst(rst), .readAddr1(rs1), .readAddr2(rs2), .readData1(rfRead1), .readData2(rfRead2), .writeEna(rfWriteEnable), .writeAddr(rfWriteAddr), .writeData(rfWriteData));
+regfile8x16 u_rf(.clk(clk), .rst(rst), .readAddr1(rs1), .readAddr2(rs2), .readData1(rfRead1), .readData2(rfRead2), .writeEna(rfWriteEnable), .writeAddr(rfWriteAddr), .writeData(rfWriteData));
 logic [15:0] aluInA, aluInB, aluResult;
 logic aluZero;
 logic [2:0] aluCtrl;
@@ -146,6 +146,7 @@ endmodule
             
             
             
+
 
 
 
